1e39b4d150a7c84f8cf1d38b55e2df8d1fe133b4
angzla <angzla329@gmail.com>
2025-03-06 15:21:20 -0800

    test

--- sql/12.sql
+++ sql/12.sql
@@ -6,3 +6,8 @@
  * Create a select statement that lists the titles of all tables with the 'Trailers' special_feature.
  * Inner join the queries above.
  */
+SELECT film.title FROM film
+JOIN (SELECT title, unnest(special_features) FROM film) as t1 USING (title)
+JOIN (SELECT title, unnest(special_features) FROM film) as t2 USING (title)
+WHERE t1.unnest = 'Trailers' AND t2.unnest = 'Behind the Scenes'
+ORDER BY film.title;
